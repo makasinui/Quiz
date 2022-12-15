6b6ea433a4e861f010272bf19d0eef9272223d96
makasinui <mks.shi@ya.ru>
2022-12-15 23:10:29 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.scss';
+import { Header } from './Components/Header';
+import Layout from './Components/Layout';
+function App()  {
+  return (
+    <div className="App">
+      <Header />
+      <Layout />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Layout.tsx
@@ -0,0 +1,17 @@
+import {Fragment} from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+
+export default function Layout () {
+    return (
+        <Fragment>
+            <CssBaseline />
+            <Container maxWidth="xl">
+                <Box sx={{ height: '100vh' }}> 
+                    
+                </Box>
+            </Container>
+        </Fragment>
+    )
+}
